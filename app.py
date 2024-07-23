c56c3bc223ce7ec57589dc074cd9c57c33849538
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2024-07-23 12:21:57 +0300

    user model

--- app.py
+++ app.py
@@ -26,3 +26,13 @@ def login():
 		# TODO: process the user credentials
 		return redirect('/index')
 	return render_template('login.html', form=form)
+
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), index=True, unique=True)
+	email = db.Column(db.String(120), index=True, unique=True)
+	password_hash = db.Column(db.String(128))
+
+	def __repr__(self):
+		return '<User {}>'.format(self.username)
