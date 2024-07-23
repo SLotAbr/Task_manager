94174f3caf7fdf13f7e7dd9211aae014c74fa965
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2024-07-23 12:12:04 +0300

    preparation to process the user credentials

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 from config import Config
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+# simplifies further updates to the database structure 
+migrate = Migrate(app, db)
 
 
-@app.route("/")
+@app.route('/')
 @app.route('/index')
 def hello_world():
 	return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	# redirects the user if the submit is successful
+	if form.validate_on_submit():
+		# TODO: process the user credentials
+		return redirect('/index')
 	return render_template('login.html', form=form)
--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
 	# A key for cryptographic purposes
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
