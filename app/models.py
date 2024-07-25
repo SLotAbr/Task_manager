d8600d8bf576ff83b7f1dc5861b4fcbd965320df
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2024-07-25 14:13:39 +0300

    task model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 
@@ -6,6 +7,21 @@ class User(db.Model):
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
+	tasks = db.relationship('Task', backref='executor', lazy='dynamic')
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
+
+
+class Task(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(140))
+	description = db.Column(db.String(2000))
+	created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	updated_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	# Is there any better db.type to store the status values?
+	status = db.Column(db.String(11), index=True, default='Created')
+	executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return '<Task {}>'.format(self.title)
