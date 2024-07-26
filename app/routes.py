18114bb288c225cc31cd3d005a0ce9fe7744c26e
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2024-07-26 13:15:00 +0300

    user registration

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 
@@ -32,3 +32,18 @@ def login():
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		# TODO: notify the client that the registration is successful
+		return redirect(url_for('login'))
+	return render_template('register.html', form=form)
