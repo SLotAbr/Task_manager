6526aca57a4672b48ee6e05fe0f452901d8ddaa7
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2024-08-01 02:41:37 +0300

    register a new task

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,10 +1,39 @@
+from flask import request, url_for
+from app import db
 from app.api import bp
-from app.models import Task
+from app.models import User, Task
 
 
 @bp.route('/tasks', methods=['POST'])
 def create_task():
-	pass
+	data = request.get_json() or {}
+	if ('title' not in data) or ('executor_id' not in data):
+		# TODO: a 400 response (bad request) with message:
+		# your request must include title and executor_id fields
+		pass
+	User.query.get_or_404(data['executor_id'])
+	is_existed = Task.query.filter_by(
+		title=data['title'], 
+		description=data['description'] if 'description' in data else '',
+		executor_id=data['executor_id']
+	).first()
+	if is_existed:
+		# TODO: a 409 response (conflict) with task_id and message:
+		# A task with given fields already exists. Its id: {}. You can use the 'PUT' method to update it
+		pass
+	else:
+		# 'title', 'executor_id' are in data; 
+		# an executor with this id is existing;
+		# the data content is original
+		task = Task()
+		data['status'] = 'Created'
+		task.from_dict(data)
+		db.session.add(task)
+		db.session.commit()
+	# This status code means that a new entity has been created. Besides, the
+	# HTTP protocol requires that a 201 response includes a `Location` header
+	# that is set to the URL of the new resource
+	return task.to_dict(), 201, {'Location': url_for('api.get_task', id=task.id)}
 
 
 @bp.route('/tasks', methods=['GET'])
