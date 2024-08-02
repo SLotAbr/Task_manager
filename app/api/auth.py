b6dba735173a2f231b770f3294d3bc92ee6b8240
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2024-08-03 02:39:01 +0300

    token authentication

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,9 +1,10 @@
-from flask_httpauth import HTTPBasicAuth
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from app.models import User
 from werkzeug.http import HTTP_STATUS_CODES
 
 
 basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
 
 
 @basic_auth.verify_password
@@ -17,5 +18,16 @@ def verify_password(username, password):
 @basic_auth.error_handler
 def basic_auth_error(status):
 	# HTTP_STATUS_CODES provides a short descriptive name for each HTTP status code
+	payload = {'error': HTTP_STATUS_CODES.get(status, 'Unknown error')}
+	return payload, status
+
+
+@token_auth.verify_token
+def verify_token(token):
+	return User.check_token(token) if token else None
+
+
+@token_auth.error_handler
+def token_auth_error(status):
 	payload = {'error': HTTP_STATUS_CODES.get(status, 'Unknown error')}
 	return payload, status
