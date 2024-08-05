b1f90f87277e9370ec0b8e4ea2a0a8f2a9ebd7d5
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2024-08-06 02:21:27 +0300

    new web pages

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.models import Task
 from flask_login import login_required
 
 
@@ -8,3 +9,15 @@ from flask_login import login_required
 @login_required
 def index():
 	return render_template('index.html')
+
+
+@app.route('/users')
+@login_required
+def users():
+	return render_template('users.html')
+
+
+@app.route('/tasks')
+@login_required
+def tasks():
+	return render_template('tasks.html')
